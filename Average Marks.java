4e92a823e1e605bdc5f70402957ab9760ac29284
Adeeb Misbahi <130211733+AdeebMisbahi1@users.noreply.github.com>
2023-05-27 19:53:52 +0530

    Average Marks

--- Average Marks.java
+++ Average Marks.java
@@ -1,6 +1,8 @@
-/* Write a program to input a name(as a single character) and marks of three tests as m1, m2, and m3 of a student considering all the three marks have been given in integer format.
+/* Write a program to input
+a name(as a single character) and marks of three tests as m1, m2, and m3 of a student considering all the three marks have been given in integer format.
 Now, you need to calculate the average of the given marks and print it along with the name as mentioned in the output format section.
-All the test marks are in integers and hence calculate the average in integer as well. That is, you need to print the integer part of the average only and neglect the decimal part.
+All the test marks are in integers and hence calculate the average in integer as well. 
+That is, you need to print the integer part of the average only and neglect the decimal part.
 
 
 
