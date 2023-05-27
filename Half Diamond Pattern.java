24e550a05bb133c876f7279a1a97ca432bb42659
Adeeb Misbahi <130211733+AdeebMisbahi1@users.noreply.github.com>
2023-05-27 22:42:39 +0530

    Half Diamond Pattern

--- Half Diamond Pattern.java
+++ Half Diamond Pattern.java
@@ -78,3 +78,50 @@ currRow += 1;
 System.out.print("*"); 
 
 } }
+// OR..
+/*
+
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println("*");
+        for (int i = 1; i <= n; i++) 
+        {
+            int j = 1;
+            System.out.print("*");
+            while (j <= i)
+            {
+                System.out.print(j);
+                j++;
+            }
+            j--;
+            while (--j >= 1)
+            {
+                System.out.print(j );
+            }
+            System.out.println("*");
+        }
+        for (int i = n - 1; i >= 1; i--) 
+        {
+            int j = 1;
+            System.out.print("*");
+            while (j <= i) {
+                System.out.print(j);
+                j++;
+            }
+            j--;
+            while (--j >= 1) 
+            {
+                System.out.print(j );
+            }
+            System.out.println("*");
+        }
+        System.out.println("*");
+    
+    }
+}
+
+*/
